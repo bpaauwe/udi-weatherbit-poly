968dc34fd0f87bed3f387b648dd3cf2f3ac2178b
Bob Paauwe <bpaauwe@yahoo.com>
2019-12-04 15:45:26 -0800

    Add more forecast data.

--- nodes/weatherbit_daily.py
+++ nodes/weatherbit_daily.py
@@ -23,20 +23,28 @@ class DailyNode(polyinterface.Node):
             {'driver': 'GV0', 'value': 0, 'uom': 4},       # high temp
             {'driver': 'GV1', 'value': 0, 'uom': 4},       # low temp
             {'driver': 'CLIHUM', 'value': 0, 'uom': 22},   # humidity
+            {'driver': 'DEWPT', 'value': 0, 'uom': 4},     # dewpoint
             {'driver': 'BARPRES', 'value': 0, 'uom': 117}, # pressure
             {'driver': 'GV13', 'value': 0, 'uom': 25},     # conditions
             {'driver': 'GV14', 'value': 0, 'uom': 22},     # clouds
             {'driver': 'GV4', 'value': 0, 'uom': 49},      # wind speed
             {'driver': 'GV5', 'value': 0, 'uom': 49},      # gust speed
+            {'driver': 'WINDDIR', 'value': 0, 'uom': 76},  # wind dir
             {'driver': 'GV6', 'value': 0, 'uom': 82},      # precipitation
+            {'driver': 'GV7', 'value': 0, 'uom': 82},      # snow
+            {'driver': 'GV8', 'value': 0, 'uom': 82},      # snow depth
             {'driver': 'GV18', 'value': 0, 'uom': 22},     # pop
             {'driver': 'GV16', 'value': 0, 'uom': 71},     # UV index
+            {'driver': 'GV10', 'value': 0, 'uom': 56},     # ozone
+            {'driver': 'GV15', 'value': 0, 'uom': 83},     # visibility
+            {'driver': 'GV9', 'value': 0, 'uom': 56},      # moon phase
             {'driver': 'GV20', 'value': 0, 'uom': 106},    # mm/day
             ]
     uom = {'GV19': 25,
             'GV0': 4,
             'GV1': 4,
             'CLIHUM': 22,
+            'DEWPT': 4,
             'BARPRES': 117,
             'GV11': 25,
             'GV12': 25,
@@ -45,11 +53,15 @@ class DailyNode(polyinterface.Node):
             'GV4': 49,
             'GV5': 49,
             'GV6': 82,
-            'GV7': 49,
-            'GV8': 49,
+            'GV7': 82,
+            'GV8': 82,
             'GV16': 71,
             'GV20': 107,
+            'GV15': 83,
             'GV18': 22,
+            'GV10': 56,
+            'GV9': 56,
+            'WINDDIR': 76,
             }
 
     def set_driver_uom(self, units):
@@ -62,9 +74,12 @@ class DailyNode(polyinterface.Node):
             self.uom['GV4'] = 49
             self.uom['GV5'] = 49
             self.uom['GV6'] = 82
-            self.uom['GV7'] = 49
-            self.uom['GV8'] = 49
+            self.uom['GV7'] = 82
+            self.uom['GV8'] = 82
             self.uom['GV20'] = 107
+            self.uom['GV15'] = 83
+            self.uom['DEWPT'] = 4
+            self.uom['WINDIR'] = 76
         elif units == 'imperial':
             self.units = 'imperial'
             self.uom['BARPRES'] = 23
@@ -74,9 +89,12 @@ class DailyNode(polyinterface.Node):
             self.uom['GV4'] = 48
             self.uom['GV5'] = 48
             self.uom['GV6'] = 105
-            self.uom['GV7'] = 48
-            self.uom['GV8'] = 48
+            self.uom['GV7'] = 105
+            self.uom['GV8'] = 105
             self.uom['GV20'] = 106
+            self.uom['GV15'] = 116
+            self.uom['DEWPT'] = 17
+            self.uom['WINDIR'] = 76
 
 
     def mm2inch(self, mm):
@@ -99,16 +117,23 @@ class DailyNode(polyinterface.Node):
 
         self.update_driver('CLIHUM', forecast['rh'])
         self.update_driver('BARPRES', forecast['pres'])
+        self.update_driver('DEWPT', forecast['dewpt'])
         self.update_driver('GV0', forecast['max_temp'])
         self.update_driver('GV1', forecast['min_temp'])
         self.update_driver('GV14', forecast['clouds'])
         self.update_driver('GV4', forecast['wind_spd'])
         self.update_driver('GV5', forecast['wind_gust_spd'])
+        self.update_driver('WINDDIR', forecast['wind_dir'])
         self.update_driver('GV6', forecast['precip'])
+        self.update_driver('GV7', forecast['snow'])
+        self.update_driver('GV8', forecast['snow_depth'])
         self.update_driver('GV19', int(dow))
         self.update_driver('GV16', forecast['uv'])
+        self.update_driver('GV15', forecast['vis'])
         self.update_driver('GV18', forecast['pop'])
-        # dewpt, snow, snow_depth, wind_dir, vis, moon_phase, ozone, 
+        self.update_driver('GV10', forecast['ozone'])
+        self.update_driver('GV9', forecast['moon_phase'])
+        # moon_phase, 
         # pod = part of day d=day, n=night
         # forecast['weather']['code']
         self.update_driver('GV13', forecast['weather']['code'])
